9d5a636281e1123ad09605951fb8287bdead6fae
acierto <bogdannechiporenko@gmail.com>
2020-02-20 12:31:50 +0100

    Update list after any operation

--- src/app/sagas/user-saga.js
+++ src/app/sagas/user-saga.js
@@ -2,21 +2,23 @@ import {all, call, put, takeEvery} from 'redux-saga/effects';
 import userActions from '../actions/user-actions';
 import {addUser, listUsers, removeUser} from '../resources/api-resource';
 
+export function* loadUsersSaga() {
+    const {data: {payload}} = yield call(listUsers);
+    yield put(userActions.set(payload));
+}
+
 export function* addUserSaga({user}) {
     yield addUser(user);
+    yield call(loadUsersSaga);
 }
 
 export function* removeUserSaga({id}) {
     yield removeUser(id);
+    yield call(loadUsersSaga);
 }
 
 export function* listUsersSaga({page}) {
     return yield listUsers(page);
-}
-
-export function* loadUsersSaga() {
-    const {data: {payload}} = yield call(listUsers);
-    yield put(userActions.set(payload));
 }
 
 export function* userSagas() {
